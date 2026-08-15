37083db796807296e4348bd5e39f56dd74d8eb61
Priyada27 <162018000+Priyada27@users.noreply.github.com>
2026-08-15 21:18:37 +0530

    Update test_risk.py

--- test_risk.py
+++ test_risk.py
@@ -1,1 +1,21 @@
+import pandas as pd
 
+from risk_engine import add_risk_analysis
+
+
+df = pd.read_csv("data/nagpur_traffic.csv")
+
+df = add_risk_analysis(df)
+
+print(
+    df[
+        [
+            "location",
+            "risk_score",
+            "risk_level"
+        ]
+    ].sort_values(
+        "risk_score",
+        ascending=False
+    )
+)
